a3e5f43feca7f9e3a5d1944d11bea1bedf0a3a65
kareemqaraeen <81602126+kareemqaraienn@users.noreply.github.com>
2023-06-28 22:07:45 -0400

    Update predict.py
    change top 3 diseases to top 1 (temp)

--- backend/predict.py
+++ backend/predict.py
@@ -29,10 +29,11 @@ def make_prediction(model_path, new_data):
     # Make a prediction
     predicted_disease = model.predict(new_data)
     predicted_probabilities = model.predict_proba(new_data)
-
     # Output the result
-    print(f'Predicted Disease: {predicted_disease[0]}')
+    # print(f'Predicted Disease: {predicted_disease[0]}')
     max_prob_index = predicted_probabilities[0].argmax()
-    print(f'Estimated Confidence: {predicted_probabilities[0][max_prob_index] * 100}%')
+    # print(f'Estimated Confidence: {predicted_probabilities[0][max_prob_index] * 100}%')
+
+    return [predicted_disease[0], predicted_probabilities[0][max_prob_index] * 100]
 
 
