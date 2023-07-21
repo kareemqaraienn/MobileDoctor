e55e2e87eaf8e8197a38e521400941216734dbc6
kareemqaraeen <81602126+kareemqaraienn@users.noreply.github.com>
2023-07-20 22:21:54 -0400

    Update predict.py
    clean code

--- backend/predict.py
+++ backend/predict.py
@@ -1,26 +1,7 @@
 import joblib
 import numpy as np
 
-# def make_prediction(model_path, new_data):
-#     # Load the model
-#     model = joblib.load(model_path)
 
-#     # Make a prediction
-#     predicted_probabilities = model.predict_proba(new_data)
-
-#     # Get the indices of the top 3 probabilities
-#     top_3_indices = np.argsort(predicted_probabilities[0])[-3:][::-1]
-
-#     # Output the top 3 diseases and their probabilities
-#     for i, index in enumerate(top_3_indices):
-#         disease = model.classes_[index]  # Assuming your model has classes_ attribute which stores the class labels
-#         probability = predicted_probabilities[0][index]
-#         print(f'Predicted Disease {i+1}: {disease}, Estimated Confidence: {probability * 100}%')
-
-
-
-# This is for printing the disease with highest probability compared to the top function that prints top 3
-# import joblib
 def make_prediction(model_path, new_data):
      
     # Load the model
